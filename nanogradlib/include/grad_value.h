908e15a9c343bed294dfacc98104e2486ae6f9f0
Thinking-Dragon <cfmgp@hotmail.com>
2023-08-17 17:07:41 -0400

    Add gradient computation when executing operations

--- nanogradlib/include/grad_value.h
+++ nanogradlib/include/grad_value.h
@@ -2,27 +2,32 @@
 
 #include <iostream>
 #include <cmath>
+#include <functional>
 
 class GradValue
 {
 public:
     GradValue(double value = 0.0);
 
-    GradValue operator+(const GradValue& other) const;
-    GradValue operator-(const GradValue& other) const;
-    GradValue operator*(const GradValue& other) const;
-    GradValue operator/(const GradValue& other) const;
-    GradValue pow(const double& exponent) const;
+    GradValue operator+(GradValue& other);
+    GradValue operator-(GradValue& other);
+    GradValue operator*(GradValue& other);
+    GradValue operator/(GradValue& other);
+    GradValue pow(double& exponent);
+    GradValue operator-();
 
     GradValue operator+(const double& other) const;
     GradValue operator-(const double& other) const;
     GradValue operator*(const double& other) const;
     GradValue operator/(const double& other) const;
 
-    GradValue relu() const;
+    GradValue relu();
 
     friend std::ostream& operator<<(std::ostream& os, const GradValue& value);
 
 private:
     double data;
+    double gradient;
+
+    std::function<void()> updateGradientBackward;
 };
--- nanogradlib/src/grad_value.cpp
+++ nanogradlib/src/grad_value.cpp
@@ -1,30 +1,74 @@
 #include "grad_value.h"
 
-GradValue::GradValue(double value) : data(value) {}
+GradValue::GradValue(double value) : data(value), gradient(0.0) {}
 
-GradValue GradValue::operator+(const GradValue& other) const
+GradValue GradValue::operator+(GradValue& other)
 {
-    return GradValue(data + other.data);
+    GradValue result = GradValue(data + other.data);
+
+    result.updateGradientBackward = [&]
+    {
+        gradient += result.gradient;
+        other.gradient += result.gradient;
+    };
+
+    return result;
 }
 
-GradValue GradValue::operator-(const GradValue& other) const
+GradValue GradValue::operator-(GradValue& other)
 {
-    return GradValue(data - other.data);
+    GradValue result = GradValue(data - other.data);
+
+    result.updateGradientBackward = [&]
+    {
+        gradient += result.gradient;
+        other.gradient -= result.gradient;
+    };
+
+    return result;
 }
 
-GradValue GradValue::operator*(const GradValue& other) const
+GradValue GradValue::operator*(GradValue& other)
 {
-    return GradValue(data * other.data);
+    GradValue result = GradValue(data * other.data);
+
+    result.updateGradientBackward = [&]
+    {
+        gradient += other.data * result.gradient;
+        other.gradient += data * result.gradient;
+    };
+
+    return result;
 }
 
-GradValue GradValue::operator/(const GradValue& other) const
+GradValue GradValue::pow(double& exponent)
 {
-    return GradValue(data / other.data);
+    GradValue result = GradValue(std::pow(data, exponent));
+
+    result.updateGradientBackward = [&]
+    {
+        gradient += (exponent * std::pow(data, exponent - 1)) * result.gradient;
+    };
+
+    return result;
 }
 
-GradValue GradValue::pow(const double& exponent) const
+GradValue GradValue::operator-()
 {
-    return GradValue(std::pow(data, exponent));
+    return GradValue(-data);
+}
+
+GradValue GradValue::operator/(GradValue& other)
+{
+    GradValue result = GradValue(data / other.data);
+
+    result.updateGradientBackward = [&]
+    {
+        gradient += result.gradient / other.data;
+        other.gradient -= (data / (other.data * other.data)) * result.gradient;
+    };
+
+    return result;
 }
 
 GradValue GradValue::operator+(const double& other) const
@@ -47,9 +91,16 @@ GradValue GradValue::operator/(const double& other) const
     return GradValue(data / other);
 }
 
-GradValue GradValue::relu() const
+GradValue GradValue::relu()
 {
-    return GradValue(data > 0 ? data : 0);
+    GradValue result = GradValue(data > 0 ? data : 0);
+
+    result.updateGradientBackward = [&]
+    {
+        gradient += (data > 0) ? result.gradient : 0;
+    };
+
+    return result;
 }
 
 std::ostream& operator<<(std::ostream& os, const GradValue& value)
